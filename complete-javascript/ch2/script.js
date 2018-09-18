d56bbea085ec1042c402c94d6496ea0965a1c4d0
Adel Moreno <adel.argenix@gmail.com>
2018-09-17 19:43:20 -0600

    added type coercion

--- complete-javascript/ch2/script.js
+++ complete-javascript/ch2/script.js
@@ -1,19 +1,33 @@
-console.log("Hello World!")
+/*
+* Variables and data types
+*/
+
+// console.log("Hello World!")
+
+// let firstName = "John";
+// console.log(firstName);
+
+// const lastName = "Smith";
+// let age = 28;
+
+// let fullAge = true;
+// console.log(fullAge);
+
+// let job;
+// console.log(job);
+
+// job = "Teacher";
+// console.log(job);
+
+// // Variable naming rules
+// let $3years = 3;
+// let johnMark = "John and Mark";
+
+/*
+* Variable mutation and type coercion
+*/
 
 let firstName = "John";
-console.log(firstName);
-
-const lastName = "Smith";
 let age = 28;
 
-let fullAge = true;
-console.log(fullAge);
-
-let job;
-console.log(job);
-
-job = "Teacher";
-console.log(job);
-
-let $3years = 3;
-let johnMark = "John and Mark";
+console.log(firstName + " " + age);
