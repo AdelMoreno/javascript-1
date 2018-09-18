5b83f5f27f6580bf6d23fba3c26f2e8d245a909d
Adel Moreno <adel.argenix@gmail.com>
2018-09-18 17:21:33 -0600

    truthy, falsy, equality operators

--- complete-javascript/ch2/script.js
+++ complete-javascript/ch2/script.js
@@ -180,49 +180,71 @@
  * ternary operator and switch statement
  */
 
- let firstName = "John";
- let age = 16;
+//  let firstName = "John";
+//  let age = 16;
 
- //ternary operator
- age >= 18 ? console.log(firstName + " drinks beer.") :
- console.log(firstName + " drinks juice.");
+//  //ternary operator
+//  age >= 18 ? console.log(firstName + " drinks beer.") :
+//  console.log(firstName + " drinks juice.");
 
- let drink = age >= 18 ? "beer" : "juice";
- console.log(drink);
+//  let drink = age >= 18 ? "beer" : "juice";
+//  console.log(drink);
 
- if (age >= 18) {
-     let drink = "beer";
+//  if (age >= 18) {
+//      let drink = "beer";
+//  } else {
+//      let drink = "juice";
+//  }
+
+// // switch statement
+// let job = "teacher";
+// switch (job) {
+//     case "teacher":
+//     case "instructor":
+//         console.log(firstName + " teaches kids how to code.");
+//         break;
+//     case "driver":
+//         console.log(firstName + " drives an uber in Lisbon.");
+//         break;
+//     case "designer":
+//         console.log(firstName + " designs beautiful website.");
+//         break;
+//     default:
+//         console.log(firstName + " does something else.");
+// }
+
+// switch (true) {
+// case age < 13:
+//     console.log(firstName + " is a boy.");
+//     break;
+// case age >= 13 && age < 20:
+//     console.log(firstName + " is a teenager.");
+//     break;
+// case age >= 20 && age < 30:
+//     console.log(firstName + " is a young man.");
+//     break;
+// default:
+//     console.log(firstName + " is a man.")
+// }
+
+/**
+ * truthy and falsy values and equality operators
+ */
+
+ // falsy values: undefined, null, 0, "", NaN
+ //truthy values: NOT falsy values
+
+ let height;
+
+ height = 23;
+
+ if (height || height === 0) {
+     console.log("Variable is defined");
  } else {
-     let drink = "juice";
+     console.log("Variable has NOT been defined");
  }
 
-// switch statement
-let job = "teacher";
-switch (job) {
-    case "teacher":
-    case "instructor":
-        console.log(firstName + " teaches kids how to code.");
-        break;
-    case "driver":
-        console.log(firstName + " drives an uber in Lisbon.");
-        break;
-    case "designer":
-        console.log(firstName + " designs beautiful website.");
-        break;
-    default:
-        console.log(firstName + " does something else.");
-}
-
-switch (true) {
-case age < 13:
-    console.log(firstName + " is a boy.");
-    break;
-case age >= 13 && age < 20:
-    console.log(firstName + " is a teenager.");
-    break;
-case age >= 20 && age < 30:
-    console.log(firstName + " is a young man.");
-    break;
-default:
-    console.log(firstName + " is a man.")
-}
+ // equality operators
+ if (height == "23") {
+     console.log("The == operator does type coercion!");
+ }
