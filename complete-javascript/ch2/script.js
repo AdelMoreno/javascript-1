566b422a086400c71d23623fe8a56e2d22d75acb
Adel Moreno <adel.argenix@gmail.com>
2018-09-17 19:35:08 -0600

    added fullAge and job

--- complete-javascript/ch2/script.js
+++ complete-javascript/ch2/script.js
@@ -5,3 +5,12 @@ console.log(firstName);
 
 const lastName = "Smith";
 let age = 28;
+
+let fullAge = true;
+console.log(fullAge);
+
+let job;
+console.log(job);
+
+job = "Teacher";
+console.log(job);
