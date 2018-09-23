d185e8990dfd4e3f2bf053160c6632c568110d72
Adel Moreno <adel.argenix@gmail.com>
2018-09-23 16:31:27 -0600

    lecture on hoisting

--- complete-javascript/ch3/script.js
+++ complete-javascript/ch3/script.js
@@ -1,10 +1,32 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+calculateAge(1990);
+
+function calculateAge(year) {
+   console.log(2016 - year); 
+}
 
 
 
+let retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
 
+
+retirement(1990);
+
+// variables
+
+let age = 23;
+console.log(age);
+
+function foo() {
+    let age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 
