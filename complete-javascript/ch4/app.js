746936528a60fc2f74aa85a52c3400733e97f89c
Adel Moreno <adel.argenix@gmail.com>
2018-09-23 19:35:32 -0600

    finished lecture 1 of game

--- complete-javascript/ch4/app.js
+++ complete-javascript/ch4/app.js
@@ -8,3 +8,29 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+let scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 1;
+
+dice = Math.floor(Math.random() * 6) + 1;
+
+
+document.querySelector("#current-" + activePlayer).textContent = dice;
+
+//document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>";
+
+
+let x = document.querySelector("#score-0").textContent;
+console.log(x);
+
+document.querySelector(".dice").style.display = "none";
+
+
+
+
+
+
+
